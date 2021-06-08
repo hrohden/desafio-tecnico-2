aaa7333079030ccd2c7d0f32da48502bbbd6b519
Henrique Broering Rohden <hrohden@gmail.com>
2021-06-08 12:49:56 -0300

    adiciona bean validation

--- backend/src/main/java/br/com/surittec/desafio/clientes/Cliente.java
+++ backend/src/main/java/br/com/surittec/desafio/clientes/Cliente.java
@@ -1,8 +1,13 @@
 package br.com.surittec.desafio.clientes;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +18,35 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotNull
+    @Size(min = 3, max = 100)
     private String nome;
+
+    @CPF
     private String cpf;
+
+    @NotNull
+    private String cep;
+
+    @NotNull
     private String logradouro;
+
+    private String complemento;
+
+    @NotNull
     private String bairro;
+
+    @NotNull
     private String cidade;
+
+    @NotNull
     private String uf;
+
+    @Email
     private String email;
 
     @OneToMany(cascade = CascadeType.ALL)
+    @Size(min = 1)
     private List<Telefone> telefones = new ArrayList<>();
 }
