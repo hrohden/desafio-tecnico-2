6ec7b18d4cde2b2ae22ebd6566c043ed4a8cef4c
Henrique Broering Rohden <hrohden@gmail.com>
2021-06-08 12:08:21 -0300

    adiciona valor gerado automaticamente para o id

--- backend/src/main/java/br/com/surittec/desafio/clientes/Cliente.java
+++ backend/src/main/java/br/com/surittec/desafio/clientes/Cliente.java
@@ -3,6 +3,8 @@ package br.com.surittec.desafio.clientes;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -10,6 +12,7 @@ import javax.persistence.Id;
 public class Cliente {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String endereco;
