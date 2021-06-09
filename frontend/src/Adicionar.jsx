b5a0a68b1a3d0ccc6bb653e65a26555fa5f78798
Henrique Broering Rohden <hrohden@gmail.com>
2021-06-09 15:17:09 -0300

    adiciona manipulação de evento

--- frontend/src/Adicionar.jsx
+++ frontend/src/Adicionar.jsx
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Adicionar extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Adicionar</h1>
-            </div>
-        )
-    }
+  static propTypes = {
+    prop: PropTypes,
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('enviou...');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Adicionar</h1>
+        <button onClick={this.handleSubmit}>Adicionar cliente</button>
+      </div>
+    );
+  }
 }
