30a46050ffb2493c76a5bd3244cc77cc7626b8fc
Henrique Broering Rohden <hrohden@gmail.com>
2021-06-09 22:11:22 -0300

    remove duplicidade de código

--- frontend/src/operacoes.js
+++ frontend/src/operacoes.js
@@ -1,41 +1,30 @@
 const urlBase = "http://localhost:3000/api/clientes/";
 
+const headers = new Headers({
+  "Content-Type": "application/json",
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBdXRlbnRpY2HDp8OjbyIsImlhdCI6MTYyMzI2MDgwMiwiZXhwIjoxNjIzMzQ3MjAyLCJzdWIiOiJhZG1pbiJ9.2NKHtgmfjn368BiZfaPKFD6I8NQwMd4jfOdHFBGNOMU",
+});
+
 export const listar = () => {
-  return fetch(urlBase, {
-    headers: new Headers({
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBdXRlbnRpY2HDp8OjbyIsImlhdCI6MTYyMzI2MDgwMiwiZXhwIjoxNjIzMzQ3MjAyLCJzdWIiOiJhZG1pbiJ9.2NKHtgmfjn368BiZfaPKFD6I8NQwMd4jfOdHFBGNOMU",
-    }),
-  }).then((res) => res.json());
+  return fetch(urlBase, { headers }).then((res) => res.json());
 };
 
 export const consultar = (id) => {
-  return fetch(urlBase + id, {
-    headers: new Headers({
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBdXRlbnRpY2HDp8OjbyIsImlhdCI6MTYyMzI2MDgwMiwiZXhwIjoxNjIzMzQ3MjAyLCJzdWIiOiJhZG1pbiJ9.2NKHtgmfjn368BiZfaPKFD6I8NQwMd4jfOdHFBGNOMU",
-    }),
-  }).then((res) => res.json());
+  return fetch(urlBase + id, { headers }).then((res) => res.json());
 };
 
 export const salvar = (body) => {
   return fetch(urlBase, {
     method: "post",
     body: JSON.stringify(body),
-    headers: new Headers({
-      "Content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBdXRlbnRpY2HDp8OjbyIsImlhdCI6MTYyMzI2MDgwMiwiZXhwIjoxNjIzMzQ3MjAyLCJzdWIiOiJhZG1pbiJ9.2NKHtgmfjn368BiZfaPKFD6I8NQwMd4jfOdHFBGNOMU",
-    }),
+    headers: headers,
   }).then((res) => res.json());
 };
 
 export const remover = (id) => {
   return fetch(urlBase + id, {
     method: "delete",
-    headers: new Headers({
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBdXRlbnRpY2HDp8OjbyIsImlhdCI6MTYyMzI2MDgwMiwiZXhwIjoxNjIzMzQ3MjAyLCJzdWIiOiJhZG1pbiJ9.2NKHtgmfjn368BiZfaPKFD6I8NQwMd4jfOdHFBGNOMU",
-    }),
+    headers: headers,
   }).then((res) => res.json());
 };
