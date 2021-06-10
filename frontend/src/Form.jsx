7bfee27df1b4f4c54a45948ad2cab13483bf75b0
Henrique Broering Rohden <hrohden@gmail.com>
2021-06-09 22:07:22 -0300

    remove duplicidade de código

--- frontend/src/Form.jsx
+++ frontend/src/Form.jsx
@@ -12,28 +12,7 @@ export default class Form extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const body = {
-      id: this.state.cliente.id,
-      nome: this.state.cliente.nome,
-      cpf: "00000000000",
-      cep: "99999999",
-      logradouro: "Teste",
-      complemento: "Teste",
-      bairro: "Teste",
-      cidade: "Teste",
-      uf: "Teste",
-      emails: [
-        {
-          endereco: "teste@teste.com.br",
-        },
-      ],
-      telefones: [
-        {
-          numero: "99999999",
-          tipo: "RESIDENCIAL",
-        },
-      ],
-    };
+    const body = { ...this.state.cliente };
     salvar(body).then(
       (result) => {
         this.setState({
